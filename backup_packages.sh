7e8cb3b4398be0c262759e7ef2cc86d22ea22858
taras <taras.serba@ukr.net>
2016-03-29 05:22:13 +0300

    Packages list backup for: date

--- backup_packages.sh
+++ backup_packages.sh
@@ -2,9 +2,10 @@
 
 GIT=`which git`
 REPO_DIR=/home/taras/SYSTEM_BACKUP/MyArch/
+COMMIT='date'
 
 pacman -Qqen > /home/taras/SYSTEM_BACKUP/MyArch/pkglist.txt
 cd ${REPO_DIR}
 ${GIT} add --all .
-${GIT} commit -m "Test commit"
+${GIT} commit -m "Packages list backup for: ${COMMIT}"
 ${GIT} push 
